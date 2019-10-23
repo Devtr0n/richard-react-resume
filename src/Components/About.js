65a1c088db21369fa1a9f2c5dec755c4e1d91d92
Richard T. Hollon <richard.hollon@xcentium.com>
2019-10-23 00:35:38 -0500

    updated resume pdf for 2020. cleaned up various items. added testimonials.

--- src/Components/About.js
+++ src/Components/About.js
@@ -40,7 +40,7 @@ class About extends Component {
                </div>
                <div className="columns download">
                   <p>
-                     <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Full Resume</a>
+                     <a href={resumeDownload} className="button" target="_blank"><i className="fa fa-download"></i>Download Full Resume</a>
                   </p>
                </div>
             </div>
