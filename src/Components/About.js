255169eca0bd8eba5d30997823dfbaf017e169ca
Richard T. Hollon <richard.hollon@xcentium.com>
2019-10-23 01:16:57 -0500

    fixed security issue with target=_blank and fixed a few tweaks/typos

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,11 +7,12 @@ class About extends Component {
       var name = this.props.data.name;
       var profilepic= "images/"+this.props.data.image;
       var bio = this.props.data.bio;
+      var bio2 = this.props.data.bio2;
+      var bio3 = this.props.data.bio3;
       var street = this.props.data.address.street;
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
       var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
       var email = this.props.data.email;
       var resumeDownload = this.props.data.resumedownload;
     }
@@ -25,22 +26,23 @@ class About extends Component {
          <div className="nine columns main-col">
             <h2>About Me</h2>
 
-            <p>{bio}</p>
+            <p>{bio}</p>            
+            <p>{bio2}</p>
+            <p>{bio3}</p>
             <div className="row">
                <div className="columns contact-details">
                   <h2>Contact Details</h2>
-                  <p className="address">
-						   <span>{name}</span><br />
-						   <span>{street}<br />
+                    <p className="address">
+						        <span>{name}</span><br />
+						        <span>{street}<br />
 						         {city} {state}, {zip}
-                   </span><br />
-						   <span>{phone}</span><br />
-                     <span>{email}</span>
+                    </span><br />
+                    <span><a href={"mailto:"+email+"?subject=Getting in touch!&body=Hi, I would like to contact you, Richard."}>{email}</a></span>
 					   </p>
                </div>
                <div className="columns download">
                   <p>
-                     <a href={resumeDownload} className="button" target="_blank"><i className="fa fa-download"></i>Download Full Resume</a>
+                     <a href={resumeDownload} className="button" target="_blank" rel="noopener noreferrer"><i className="fa fa-download"></i>Download Full Resume</a>
                   </p>
                </div>
             </div>
