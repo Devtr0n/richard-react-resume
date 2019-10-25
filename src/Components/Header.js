1eea9d02211be4296f14c8252222a263e66bc50a
Richard T. Hollon <richard.hollon@xcentium.com>
2019-10-25 15:40:40 -0500

    fixed font icons for arrows

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -44,7 +44,7 @@ class Header extends Component {
       </div>
 
       <p className="scrolldown">
-         <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
+         <a className="smoothscroll" href="#about"><i className="fa fa-chevron-circle-down"></i></a>
       </p>
 
    </header>
